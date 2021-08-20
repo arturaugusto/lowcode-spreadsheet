f3258e8250f8840decb5a374912a4fa1f682cc86
Artur Augusto Martins <arturaugusto@gmail.com>
2021-08-19 22:02:06 -0300

    crud working

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@
         </header>
         <section class="modal-card-body">
 
-          {{model}}
+          <!-- {{model}} -->
 
           <div class="field">
             <label class="label">Name</label>
@@ -19,9 +19,9 @@
               <input v-model="model.name" class="input" type="text" placeholder="Model name">
             </div>
           </div>
-          {{funcs}}
+          <!-- {{ funcs }} -->
           <span v-if="funcs && funcs.length" class="select is-primary field mr-2">
-            <select v-model="selFuncId">
+            <select @change="onFuncSelChange($event)">
               <option v-for="func in funcs" :key="func.id" :value="func.id">{{func.name}}</option>
             </select>
           </span>
@@ -30,29 +30,30 @@
             <button @click="createFunc" class="button is-success">New function</button>
           </span>
 
-          <div v-if="selFunc">
+          <div v-if="func" class="box has-background-white-ter">
             <div class="columns is-desktop">
 
               <div class="column is-10">
                 <div class="control">
                   <label class="label">Function name</label>
-                  <input v-model="selFunc.name" class="input" type="text" placeholder="Function name">
+                  <input v-model="func.name" class="input" type="text" placeholder="Function name">
                 </div>
               </div>
 
               <div class="column is-2">
                 <div class="control">
                   <label class="label">Unit</label>
-                  <input v-model="selFunc.unit" class="input" type="text" placeholder="Unit" style="max-width: 128px;">
+                  <input v-model="func.unit" class="input" type="text" placeholder="Unit" style="max-width: 128px;">
                 </div>
               </div>
             </div>
 
-            <!-- <div class="">
+            <div class="">
               <HelloWorld
-                :schema="{'cols': [{name: 'A', type: 'list', options: ['lala', 'bah']}, {name: 'B', type: 'string'}]}"
+                :schema="{'cols': [{name: 'A', type: 'list', options: ['lala', 'bah']}, {name: 'B', type: 'string'},{name: 'C', type: 'string'},{name: 'D', type: 'string'}]}"
               />
-            </div> -->
+            </div>
+            <button @click="saveItem('func')" class="button is-success">Save changes</button>
           </div>
 
           <!-- Content ... -->
@@ -92,7 +93,7 @@
             </span>
           </p>
         </div>
-        <a v-for="model in models" :key="model.id" class="panel-block" @click="editItem('model', model.id)">
+        <a v-for="model in models" :key="model.id" class="panel-block" @click="pull('model', model.id)">
           <span class="panel-icon">
             <i class="fas fa-book" aria-hidden="true"></i>
           </span>
@@ -105,7 +106,7 @@
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue'
 import PouchDB from 'pouchdb'
 import pouchdbUpsert from 'pouchdb-upsert'
 import relationalPouch from 'relational-pouch'
@@ -120,7 +121,7 @@ import timeToId from './timeToId.js'
 export default {
   name: 'App',
   components: {
-    // HelloWorld
+    HelloWorld
   },
   mounted () {
     this.db = new PouchDB('my_database')
@@ -160,6 +161,7 @@ export default {
     })
 
 
+    // listen changes and manage _rev for local data
     this.db.changes({
       since: 'now',
       include_docs: true,
@@ -169,7 +171,7 @@ export default {
       let type = docInfoObj.type
       let id = docInfoObj.id
       if (this[type] && this[type].id === id) {
-        console.log(change)
+        // console.log(change)
         // Object.assign(this[type], change.doc.data)
         this[type] = change.doc.data
         this[type].id = id
@@ -179,6 +181,9 @@ export default {
 
   },
   methods: {
+    onFuncSelChange (event) {
+      this.func = this.funcs.filter(item => item.id === event.target.value)[0]
+    },
     cancelItemEdit (type) {
       return this.db.rel.find(type).then((response) => {
         this[type] = null
@@ -191,10 +196,14 @@ export default {
         console.warn(type + ' is not avaliable on `this`.')
         return
       }
-      
-      return this.db.rel.save(type, this[type]).then((response) => {
-        // this[type].rev = response.rev
-        console.log(response)
+      // upsert data
+      return this.db.rel.find(type, this[type].id).then(response => {
+        let plural = this.schema.filter(item => item.singular === type)[0].plural
+        if (JSON.stringify(response[plural][0]) !== JSON.stringify(this[type])) {
+          return this.db.rel.save(type, this[type])
+        }
+      }).then(() => {
+        // console.log(response)
       }).catch(function (err) {
         if (err.code === 409) { // conflict
           // handle the conflict
@@ -203,50 +212,37 @@ export default {
         }
       })
     },
-    editItem (type, ids) {
-
-      const traverse = (target, ids, action) => {
-        action = action === undefined ? 'find' : 'save'
-        
+    pull (type, ids) {
+      const traverse = (target, ids) => {
         let targetSchema = this.schema.filter(item => item.singular === target)[0]
 
-        const traverseNext = (response) => {
+        this.db.rel.find(target, ids).then((response) => {
+
+          let found
+          if (ids && ids.constructor === Array) {
+            this[targetSchema.plural] = response[targetSchema.plural]
+            found = response[targetSchema.plural]
+            this[targetSchema.singular] = response[targetSchema.plural][0]
+          }
+
+          if (ids && ids.constructor === String) {
+            this[target] = response[targetSchema.plural][0]
+            found = response[targetSchema.plural][0]
+          }
+
           let relations = targetSchema.relations
           Object.keys(relations).forEach(relation => {
             if (relations[relation]['hasMany']) {
               let targetSingular = relations[relation]['hasMany']['type']
-              if (ids && ids.constructor === String && response[targetSchema.plural][0]) {
-                if (response[targetSchema.plural][0][relation]) {
-                  traverse(targetSingular, response[targetSchema.plural][0][relation], action)
-                }
-              }
+              let targetPlural = this.schema.filter(item => item.singular === targetSingular)[0].plural
+              traverse(targetSingular, found[targetPlural])
             }
           })
-        }
-
-        if (action === 'find') {
-          this.db.rel.find(target, ids).then((response) => {
-            
-            if (ids && ids.constructor === Array) {
-              this[targetSchema.plural] = response[targetSchema.plural]
-            }
-            if (ids && ids.constructor === String) {
-              this[target] = response[targetSchema.plural][0]
-            }
-            traverseNext(response)
-          })
-        }
-
-        if (action === 'save') {
-          this.db.rel.save(target, ids).then((response) => {
-            traverseNext(response)
-          })
-        }
+        })
       }
       traverse(type, ids)
     },
 
-    
     // factory functions
     
     createModel () {
@@ -254,21 +250,25 @@ export default {
         id: timeToId.toB64(),
         name: '',
       }).then((response) => {
-        this.editItem('model', response.id)
+        this.pull('model', response.id)
       }).catch(err => {
         console.log(err)
       })
     },
     createFunc () {
       let id = timeToId.toB64()
+      let func = {
+        id: id,
+        name: '',
+        unit: '',
+        model: this.model.id,
+      }
+      this.model.funcs = this.model.funcs || []
+      this.model.funcs.push(id)
       return this.saveItem('model').then(() => {
-        return this.db.rel.save('func', {
-          id: id,
-          name: '',
-          unit: '',
-          model: this.model.id,
-        })
+        return this.db.rel.save('func', func)
       }).then((response) => {
+        this.pull('func', this.model.funcs)
         console.log(response)
       }).catch(err => {
         console.log(err)
@@ -276,16 +276,13 @@ export default {
     },
   },
   computed: {
-    selFunc () {
-      return this.funcs.filter(func => func.id === this.selFuncId)[0]
-    }
   },
   data () {
     return {
       model: null,
       models: [],
+      func: null,
       funcs: [],
-      selFuncId: undefined,
     }
   },
 }
